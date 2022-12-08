97df591f55da4c13984eee801417762ac6c653bf
satoshiwasHere <skinyanjui100@gmail.com>
2022-12-08 10:45:39 -0800

    Task_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - main block
- * Return: o
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
