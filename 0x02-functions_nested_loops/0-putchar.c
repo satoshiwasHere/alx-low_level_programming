7931011b0e8d2d3e2e9467fd0f9668f86e40aaa3
satoshiwasHere <skinyanjui100@gmail.com>
2022-12-14 10:42:25 -0800

    Task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include
+
+/**
+ * main - prints _putchar and a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	-putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h:
+#include <unistd.h>
+/**
+ * _Putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
