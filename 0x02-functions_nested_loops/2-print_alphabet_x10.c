5fa940c4e56ee0c6560cd4d850ac705eda859a1f
satoshiwasHere <skinyanjui100@gmail.com>
2022-12-15 01:57:46 -0800

    Task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints all alphabets in lowercase 10 times.
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
