2176b84b619d953dac79d7f9de7b585de6d565f4
satoshiwasHere <skinyanjui100@gmail.com>
2022-12-15 00:26:38 -0800

    T1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphanet
+ * return void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
