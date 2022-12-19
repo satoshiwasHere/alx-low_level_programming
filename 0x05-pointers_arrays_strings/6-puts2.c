ae045b6f50414f8fcfa07194b1e0fa7e23e2f83f
satoshiwasHere <skinyanjui100@gmail.com>
2022-12-19 15:23:42 -0800

    task7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * put2 - function printing every other character of a string
+ * starting wuth the first character, followed by  new line
+ *
+ * Return: void.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
