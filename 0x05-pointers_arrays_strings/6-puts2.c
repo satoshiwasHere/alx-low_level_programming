bf8f4e97598c8a987c8684f1abd20d87fef3710c
satoshiwasHere <skinyanjui100@gmail.com>
2022-12-19 16:18:54 -0800

    Task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 /**
  * put2 - function printing every other character of a string
  * starting wuth the first character, followed by  new line
- *
+ *@ str: pointer to string.
  * Return: void.
  */
 void puts2(char *str)
