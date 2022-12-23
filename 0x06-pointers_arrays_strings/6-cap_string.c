8bb5a77bad7f9d0e473f522882b6738b706cc8a8
satoshiwasHere <skinyanjui100@gmail.com>
2022-12-23 13:58:39 -0800

    Task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,6 @@ char *cap_string(char *str)
 
 			if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
-			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
 			str[index - 1] == ',' ||
 			str[index - 1] == ';' ||
